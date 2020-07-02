6b91bae297e110a6489034aad57c5fc74632d34c
Kevin Elaba <kevine.@Kevins-MacBook-Pro-2.local>
2020-07-02 11:26:37 -0400

    check

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 
 const port = process.env.port || 8081;
 
-app.get('/', function(req, res) {
-    res.send('hello world');
-  });
-
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
